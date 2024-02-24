2374488228d667d586ea7acadd990951a5d2cf62
Sharon-SHH <sharonheng.shi@gmail.com>
2024-02-24 03:26:18 -0500

    add and search city, then display local weather

---
+++ func/weather.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express();
+const axios = require("axios");
+KEY = "c135c35a2372db5080966e06437bf429";
+
+const cities = async (req, res) => {
+    try {
+        const input = req.query.search;
+        const locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit=5&appid=${KEY}`;
+        const response = await axios.get(locationUrl);
+        console.log(response.data);
+        const cityData = response.data;
+        if(!cityData) {
+            return res.status(400).json({error: "No such city"});
+        }
+        // count country: if multiple cities have the same name in a country, "name,state,country"
+        const stateCount = {};
+        cityData.forEach(element => {
+            stateCount[element.country] =
+              (stateCount[element.country] || 0) + 1;
+        });
+        const selectedOptions = cityData
+          .map((item) => {
+            // when the multiple cities have the same name in the country
+            if (stateCount[item.country] > 1) {
+              return `${item.name},${item.state || ""},${item.country}`;
+            } else {
+              return `${item.name},${item.country}`;
+            }
+          });
+        const sortedOptions = selectedOptions.sort();
+        res.json(sortedOptions);
+    } catch (error) {
+        res.status(500).json({error: `${error}: Server doesn't work`});
+    }
+   
+}
+const weatherData = async (req, res) => {
+    try {
+        const city = req.query.search;
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`;
+        const response = await axios.get(weatherUrl);
+        console.log(`weath: ${response}`);
+        const weather = response.data;
+        if (!weather) {
+            return res.status(400).json({ error: "No such city" });
+        }
+        console.log(weather);
+        res.status(200).json(weather);
+
+    } catch (error) {
+        res.status(500).json({ error: `${error}: Server doesn't work` });
+    }
+}
+router.get("/city", cities);
+router.get("/", weatherData);
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -2,10 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const weather = require("./func/weather");
 const news = require("./func/news");
 PORT = process.env.PORT || 8080;
 app.use(cors());
 
+app.use("/weather", weather);
 app.use("/news", news);
 
 app.listen(PORT, ()=>{
